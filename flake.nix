fd7d192e035305eec676adc8579aa6f7dcff26b4
r3z-sys <rezky@atmaluhur.ac.id>
2025-05-28 20:07:17 +0700

    test

--- flake.nix
+++ flake.nix
@@ -20,10 +20,10 @@
 
         oci8Ext = php.buildPecl {
           pname = "oci8";
-          version = "3.2.1";
+          version = "3.4.0";
           src = pkgs.fetchurl {
-            url = "https://pecl.php.net/get/oci8-3.2.1.tgz";
-            sha256 = "zyF703DzRZDBhlNFFt/dknmZ7layqhgjG1/ZDN+PEsg=";
+            url = "https://pecl.php.net/get/oci8-3.4.0.tgz";
+            sha256 = "sha256-YPXDijyQxGHZbWHFEpx4xTq3hCJU3ANVIi5t0OqMEag=";
           };
           buildInputs = [ oracleClient pkgs.pkg-config pkgs.libaio  ];
           # Untuk runtime, tambahkan propagatedBuildInputs supaya libaio ada saat load oci8.so
